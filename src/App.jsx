819c05940aee29776cff97bd7fad661674edc81a
Amisha-0505 <amishashaw375@gmail.com>
2024-04-22 10:26:27 +0530

    Create pages anf routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import About from './pages/About'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-cyan-600">
-      Hello world!
-    </h1>
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/signin' element={<SignIn/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      <Route path='/about' element={<About/>}/>
+    </Routes>
+   </BrowserRouter>
   )
 }
 
