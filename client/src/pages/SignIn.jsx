5baeebab333f2207d73aab31d7bb6d4f919aada3
Amisha-0505 <amishashaw375@gmail.com>
2024-05-03 22:54:26 +0530

    complete sign in functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,8 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link ,useNavigate} from 'react-router-dom';
+import axios from 'axios'
 
 function SignIn() {
+  const [formData,setFormData]=useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate=useNavigate();
+
+  const handleChange=(e)=>{
+    setFormData({
+      ...formData,
+      [e.target.id]:e.target.value
+    })
+  };
+  // console.log(formData)
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res=await axios.post(`http://localhost:3000/api/auth/signin`, 
+        {formData}
+      );
+      // console.log(response);
+      const data = await res.data;
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/');
+    } catch (error) {
+      setLoading(false);
+      setError(error.response.data.message);
+    }
+  }
   return (
-    <div>SignIn</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input
+          type='email'
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
+        />
+
+        <button disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign In'}
+        </button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Dont have an account?</p>
+        <Link to={'/sign-up'}>
+          <span className='text-blue-700'>Sign Up</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
+      {/* <p className='text-red-700 mt-5'>{error ? error.message || 'something wend wrong!':''}</p> */}
+      {/* <p className='text-red-700 mt-5'>{error.message}</p> */}
+    </div>
   )
 }
 
